ae16a3157c8d3e39d56e7f49d01da3ba44a092f4
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-07-10 06:22:33 +0800

    08_calculator, doneclear

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	return num.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  return num.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  return num * factorial(num - 1);
 };
+
+console.log(factorial(10));
 
 // Do not edit below this line
 module.exports = {
