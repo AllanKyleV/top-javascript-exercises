7d3c8f426fd35c3badbfae0a6ef0300c5873cf1a
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-07-10 19:15:14 +0800

    started

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function(people) {
+    
 };
+
+// Pseducode:
+// Goal: Return the name of the oldest person from the given array data.
+// 
+
+console.log(people(people).name); // Ray
 
 // Do not edit below this line
 module.exports = findTheOldest;
