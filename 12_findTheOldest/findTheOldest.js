17af77e7f9f1bdcc41c911d53e1d5e21e728daa2
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-07-11 19:17:42 +0800

    all done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -17,14 +17,37 @@ const people = [
     ]
 
 const findTheOldest = function(people) {
-    
+  let persons = [];
+
+  for (let person of people) {
+    let property = {};
+
+    property['name'] = person.name;
+    if (person.yearOfDeath === undefined) {
+      property['age'] = new Date().getFullYear() - person.yearOfBirth;
+    } else if (person.yearOfBirth === undefined) {
+      property['age'] = person.yearOfDeath - new Date().getFullYear();
+    } else {
+      property['age'] = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    persons.push(property);
+  }
+
+  const oldest = persons.reduce((oldestSoFar, currentPerson) => {
+    return currentPerson.age > oldestSoFar.age ? currentPerson : oldestSoFar;
+  });
+
+  return oldest;
 };
 
 // Pseducode:
 // Goal: Return the name of the oldest person from the given array data.
-// 
+// Loop the array of objects, get the age of each person, age = yearOfBirth - yearOfDeath.
+// We need an array of person, with their name and age as properties.
 
-console.log(people(people).name); // Ray
+console.log(findTheOldest(people));
+console.log(findTheOldest(people).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
